dab99bd09862a011ac91f39b72e2aeef187b4fd9
Marek Musinsky <marek.musinsky@gmail.com>
2025-01-22 17:16:40 +0100

    Calendar halwfay done

--- app/script.py
+++ app/script.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
+def convert_czech_date_to_iso(czech_date):
+    
+    parsed_date = datetime.datetime.strptime(czech_date, "%d. %B %Y")
+    
+    iso_date = parsed_date.strftime(f"%Y-%m-%d")
+    return iso_date
 
 def scraping_using_python_requests():
     """
@@ -20,10 +27,22 @@ def scraping_using_python_requests():
     soup = BeautifulSoup(html_content, "html.parser")
     table = soup.find("table", class_="mtr-table mtr-tr-th")
     rows = table.find_all("tr")
+    events_raw = []
     for row in rows[1:]:
         date = row.find_all("div", class_="mtr-cell-content")[0].find("span").decode_contents()
-        name_of_action = row.find_all("div", class_="mtr-cell-content")[1].find("span").decode_contents()
-        print(date, name_of_action)
+        name_of_event = row.find_all("div", class_="mtr-cell-content")[1].find("span").decode_contents()
+        events_raw.append((date, name_of_event))
+    events = []
+    for event in events_raw:
+        date = convert_czech_date_to_iso(event[0])
+        if "-" in date:
+            days = date.split(" ")[:3].split("-")
+            ending = date.split(" ")[3:]
+            start = days[0] + " " + ending
+            end = days[1] + " " + ending
+        name = event[1]
+        events.append({"summary": name, "start": {"date": date}, "end": {"date": date}})
 
+        
 if __name__ == '__main__':
     scraping_using_python_requests()
