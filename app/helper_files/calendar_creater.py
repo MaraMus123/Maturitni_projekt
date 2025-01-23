5255728e32fe13c5ae9972034e8a258cd85df840
Marek Musinsky <marek.musinsky@gmail.com>
2025-01-23 09:12:18 +0100

    Calendar feature implemented

--- app/helper_files/calendar_creater.py
+++ app/helper_files/calendar_creater.py
@@ -10,35 +10,39 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def authenticate_google_calendar():
     creds = None
-    
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+
+    if os.path.exists('/app/credentials/token.pickle'):
+        with open('/app/credentials/token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    
+    else:
+        raise FileNotFoundError("Token not found")
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            
-            flow = InstalledAppFlow.from_client_secrets_file('/app/credentials/client_secret_calendar.json', SCOPES)
-            creds = flow.run_console()  
-        
+
+            flow = InstalledAppFlow.from_client_secrets_file('/app/credentials/client_secret_calendar_api.json', SCOPES)
+            creds = flow.run_console()
+
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
-    
+
     service = build('calendar', 'v3', credentials=creds)
     return service
 
 def create_new_events(service, event_list):
     for event in event_list:
         calendar_id = "64952e8c1a7313576772a16d374474b3995188aea30825d9d168f11cb5f0f25e@group.calendar.google.com"
+        print(event)
         event = service.events().insert(calendarId=calendar_id, body=event).execute()
         print('Event created: %s' % (event.get('htmlLink')))
+        break
 
 def create_calendar(service, calendar_name):
     calendar = {
         'summary': calendar_name,
-        'timeZone': 'Europe/Berlin', 
+        'timeZone': 'Europe/Berlin',
     }
     created_calendar = service.calendars().insert(body=calendar).execute()
     print(f"Calendar created: {created_calendar['id']}")
@@ -47,7 +51,7 @@ def create_calendar(service, calendar_name):
 def calendar_add_events(event_list):
     service = authenticate_google_calendar()
     create_new_events(service, event_list)
-    
+
     return "All the events successfully created!"
 
-    
+
--- app/script.py
+++ app/script.py
@@ -1,12 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from helper_files.calendar_creater import calendar_add_events
 
 def convert_czech_date_to_iso(czech_date):
-    
-    parsed_date = datetime.datetime.strptime(czech_date, "%d. %B %Y")
-    
-    iso_date = parsed_date.strftime(f"%Y-%m-%d")
+
+    month_map = {
+        "leden": "01", "únor": "02", "březen": "03", "duben": "04",
+        "květen": "05", "červen": "06", "červenec": "07", "srpen": "08",
+        "září": "09", "říjen": "10", "listopad": "11", "prosinec": "12"
+    }
+        # Split the date string
+    parts = czech_date.split()
+    day = parts[0].strip(".")
+    month = month_map[parts[1].lower()]
+    year = parts[2]
+    # Construct ISO 8601 string
+    iso_date = f"{year}-{month}-{day}"
     return iso_date
 
 def scraping_using_python_requests():
@@ -34,15 +44,26 @@ def scraping_using_python_requests():
         events_raw.append((date, name_of_event))
     events = []
     for event in events_raw:
-        date = convert_czech_date_to_iso(event[0])
-        if "-" in date:
-            days = date.split(" ")[:3].split("-")
-            ending = date.split(" ")[3:]
-            start = days[0] + " " + ending
-            end = days[1] + " " + ending
+        date = event[0]
+        if "a" in date.split(" "):
+            continue
+        try:
+            if "–" in date:
+                days = "".join(date.split(" ")[:3]).split("–")
+                ending = " ".join(date.split(" ")[3:])
+                start = days[0] + " " + ending
+                end = days[1] + " " + ending
+            else:
+                start = convert_czech_date_to_iso(event[0])
+        except:
+            continue
         name = event[1]
-        events.append({"summary": name, "start": {"date": date}, "end": {"date": date}})
+        events.append({"summary": name,
+                       "start": {"date": start, 'timeZone': 'Europe/Berlin'},
+                       "end": {"date": start, 'timeZone': 'Europe/Berlin'}})
+    print(calendar_add_events(events))
 
-        
+
+
 if __name__ == '__main__':
     scraping_using_python_requests()
